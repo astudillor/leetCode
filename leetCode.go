26f3477802b0845edfbdb4971f543d39c0223cc6
rastudilloreng <r.a.astudillo@tudelft.nl>
2020-07-09 15:47:04 +0200

    new docmentation package file

--- leetCode.go
+++ leetCode.go
@@ -1,28 +1,7 @@
 package leetCode
 
-import "fmt"
+/*
+	These are the implementation of the data structures to solve problems in
+	leetcode.com
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
-func (head *ListNode) String() string {
-	if head == nil {
-		return "-> //"
-	}
-	return fmt.Sprintf("%d -> ", head.Val) + head.Next.String()
-}
-
-func Ints2List(nums ...int) *ListNode {
-	if len(nums) == 0 {
-		return nil
-	}
-	head := ListNode{Val: nums[0]}
-	lastNode := &head
-	for _, n := range nums[1:] {
-		lastNode.Next = &ListNode{Val: n}
-		lastNode = lastNode.Next
-	}
-	return &head
-}
+*/
