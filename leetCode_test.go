b5f8675ec8b873e108ba5e4ebd4e1ad1c3c88bdd
rastudilloreng <r.a.astudillo@tudelft.nl>
2020-07-09 15:48:04 +0200

    test for Ints2List added

--- leetCode_test.go
+++ leetCode_test.go
@@ -4,9 +4,18 @@ import "testing"
 
 func TestEmptyListPrint(t *testing.T) {
 	// t.Fatal("not implemented")
-	want := "-> //"
+	want := "//"
 	var nodeNil *ListNode
 	if got := nodeNil.String(); got != want {
+		t.Errorf("nodeNil.String() = %q, want %q", got, want)
+	}
+}
+
+func TestInts2List(t *testing.T) {
+	// t.Fatal("not implemented")
+	want := "1 -> 2 -> 3 -> 6 -> //"
+	head := Ints2List(1, 2, 3, 6)
+	if got := head.String(); got != want {
 		t.Errorf("nodeNil.String() = %q, want %q", got, want)
 	}
 }
